a53b63dbda55b4e9a1a11f4e252a29dddb162098
jcb-cummings <jacob_cummings@yahoo.com>
2019-09-15 02:18:40 -0500

    Update instagram_download.bat

--- instagram_download.bat
+++ instagram_download.bat
@@ -1,7 +1,10 @@
 @echo off
 set /p scrap_ID="Enter the Instagram user ID you wish to scrape: "
 @echo on
-REM If you have only one instagram account, please edit this Batch file with your login details.
+REM If you do not need an Instagram account to access this account, press enter.
+REM If you have only one Instagram account, 
+REM please edit this Batch file with your login details inside the quotation marks for easier usage.
+REM When done, remove the "/p" from between set and the variables.
 @echo off
 set /p user_ID="Enter your Instagram user ID: "
 set /p user_PW="Enter your Instagram password: "
